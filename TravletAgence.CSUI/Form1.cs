84d26704a4dad27c5068e84dca56846196fd60a9
YXP <954222619@qq.com>
2017-09-30 10:33:06 +0800

    修改CSUI 为DotNet FrameWork4.0 兼容winxp

--- TravletAgence.CSUI/Form1.cs
+++ TravletAgence.CSUI/Form1.cs
@@ -4,10 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TravletAgence.Model;
 
--- TravletAgence.CSUI/IDCard.cs
+++ TravletAgence.CSUI/IDCard.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TravletAgence.Model;
 
