69f6a79fca5b158a105437f6218a81ea61b9b28a
YXP <954222619@qq.com>
2017-10-21 17:12:19 +0800

    添加合并单元格demo,但是不合适，最终放弃，下一步加一个批量设置即可

---
+++ DgvMergeDemo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DgvMergeDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1010, 503);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridViewX1_CellPainting);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 551);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.DataGridViewX dataGridView1;
+    }
+}
+
---
+++ DgvMergeDemo/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DgvMergeDemo
+{
+    public partial class Form1 : Form
+    {
+        //需要(行、列)合并的所有列标题名
+        List<String> colsHeaderText_V = new List<String>();
+        List<String> colsHeaderText_H = new List<String>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            colsHeaderText_H.Add("Birthplace");
+        }
+
+        //cell painting的方式不好，不能居中而且不能选中合并后的单元格
+        private void dataGridViewX1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+        //    foreach (string fieldHeaderText in colsHeaderText_H)
+        //    {
+        //        //纵向合并
+        //        if (e.ColumnIndex >= 0 && this.dataGridView1.Columns[e.ColumnIndex].HeaderText == fieldHeaderText && e.RowIndex >= 0)
+        //        {
+        //            using (
+        //                Brush gridBrush = new SolidBrush(this.dataGridView1.GridColor),
+        //                backColorBrush = new SolidBrush(e.CellStyle.BackColor))
+        //            {
+        //                using (Pen gridLinePen = new Pen(gridBrush))
+        //                {
+        //                    // 擦除原单元格背景
+        //                    e.Graphics.FillRectangle(backColorBrush, e.CellBounds);
+
+        //                    /****** 绘制单元格相互间隔的区分线条，datagridview自己会处理左侧和上边缘的线条，因此只需绘制下边框和和右边框
+        //                     DataGridView控件绘制单元格时，不绘制左边框和上边框，共用左单元格的右边框，上一单元格的下边框*****/
+
+        //                    //不是最后一行且单元格的值不为null
+        //                    if (e.RowIndex < this.dataGridView1.RowCount - 1 && this.dataGridView1.Rows[e.RowIndex + 1].Cells[e.ColumnIndex].Value != null)
+        //                    {
+        //                        //若与下一单元格值不同
+        //                        if (e.Value.ToString() != this.dataGridView1.Rows[e.RowIndex + 1].Cells[e.ColumnIndex].Value.ToString())
+        //                        {
+        //                            //下边缘的线
+        //                            e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left, e.CellBounds.Bottom - 1,
+        //                            e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+        //                            //绘制值
+        //                            if (e.Value != null)
+        //                            {
+        //                                e.Graphics.DrawString(e.Value.ToString(), e.CellStyle.Font,
+        //                                    Brushes.Crimson, e.CellBounds.X + 2,
+        //                                    e.CellBounds.Y + 2, StringFormat.GenericDefault);
+        //                            }
+        //                        }
+        //                        //若与下一单元格值相同 
+        //                        else
+        //                        {
+        //                            //背景颜色
+        //                            //e.CellStyle.BackColor = Color.LightPink;   //仅在CellFormatting方法中可用
+        //                            this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.LightBlue;
+        //                            this.dataGridView1.Rows[e.RowIndex + 1].Cells[e.ColumnIndex].Style.BackColor = Color.LightBlue;
+        //                            //只读（以免双击单元格时显示值）
+        //                            this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
+        //                            this.dataGridView1.Rows[e.RowIndex + 1].Cells[e.ColumnIndex].ReadOnly = true;
+        //                        }
+        //                    }
+        //                    //最后一行或单元格的值为null
+        //                    else
+        //                    {
+        //                        //下边缘的线
+        //                        e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left, e.CellBounds.Bottom - 1,
+        //                            e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+
+        //                        //绘制值
+        //                        if (e.Value != null)
+        //                        {
+        //                            e.Graphics.DrawString(e.Value.ToString(), e.CellStyle.Font,
+        //                                Brushes.Crimson, e.CellBounds.X + 2,
+        //                                e.CellBounds.Y + 2, StringFormat.GenericDefault);
+        //                        }
+        //                    }
+
+        //                    ////左侧的线（）
+        //                    //e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left,
+        //                    //    e.CellBounds.Top, e.CellBounds.Left,
+        //                    //    e.CellBounds.Bottom - 1);
+
+        //                    //右侧的线
+        //                    e.Graphics.DrawLine(gridLinePen, e.CellBounds.Right - 1,
+        //                        e.CellBounds.Top, e.CellBounds.Right - 1,
+        //                        e.CellBounds.Bottom - 1);
+
+        //                    //设置处理事件完成（关键点），只有设置为ture,才能显示出想要的结果。
+        //                    e.Handled = true;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    foreach (string fieldHeaderText in colsHeaderText_V)
+        //    {
+        //        //横向合并
+        //        if (e.ColumnIndex >= 0 && this.dataGridView1.Columns[e.ColumnIndex].HeaderText == fieldHeaderText && e.RowIndex >= 0)
+        //        {
+        //            using (
+        //                Brush gridBrush = new SolidBrush(this.dataGridView1.GridColor),
+        //                backColorBrush = new SolidBrush(e.CellStyle.BackColor))
+        //            {
+        //                using (Pen gridLinePen = new Pen(gridBrush))
+        //                {
+        //                    // 擦除原单元格背景
+        //                    e.Graphics.FillRectangle(backColorBrush, e.CellBounds);
+
+        //                    /****** 绘制单元格相互间隔的区分线条，datagridview自己会处理左侧和上边缘的线条，因此只需绘制下边框和和右边框
+        //                     DataGridView控件绘制单元格时，不绘制左边框和上边框，共用左单元格的右边框，上一单元格的下边框*****/
+
+        //                    //不是最后一列且单元格的值不为null
+        //                    if (e.ColumnIndex < this.dataGridView1.ColumnCount - 1 && this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex + 1].Value != null)
+        //                    {
+        //                        if (e.Value.ToString() != this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex + 1].Value.ToString())
+        //                        {
+        //                            //右侧的线
+        //                            e.Graphics.DrawLine(gridLinePen, e.CellBounds.Right - 1, e.CellBounds.Top,
+        //                                e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+        //                            //绘制值
+        //                            if (e.Value != null)
+        //                            {
+        //                                e.Graphics.DrawString(e.Value.ToString(), e.CellStyle.Font,
+        //                                    Brushes.Crimson, e.CellBounds.X + 2,
+        //                                    e.CellBounds.Y + 2, StringFormat.GenericDefault);
+        //                            }
+        //                        }
+        //                        //若与下一单元格值相同 
+        //                        else
+        //                        {
+        //                            //背景颜色
+        //                            //e.CellStyle.BackColor = Color.LightPink;   //仅在CellFormatting方法中可用
+        //                            this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.LightPink;
+        //                            this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex + 1].Style.BackColor = Color.LightPink;
+        //                            //只读（以免双击单元格时显示值）
+        //                            this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
+        //                            this.dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex + 1].ReadOnly = true;
+        //                        }
+        //                    }
+        //                    else
+        //                    {
+        //                        //右侧的线
+        //                        e.Graphics.DrawLine(gridLinePen, e.CellBounds.Right - 1, e.CellBounds.Top,
+        //                            e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+
+        //                        //绘制值
+        //                        if (e.Value != null)
+        //                        {
+        //                            e.Graphics.DrawString(e.Value.ToString(), e.CellStyle.Font,
+        //                                Brushes.Crimson, e.CellBounds.X + 2,
+        //                                e.CellBounds.Y + 2, StringFormat.GenericDefault);
+        //                        }
+        //                    }
+        //                    //下边缘的线
+        //                    e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left, e.CellBounds.Bottom - 1,
+        //                                                e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+        //                    e.Handled = true;
+        //                }
+        //            }
+
+        //        }
+        //    }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TravletAgence.BLL.VisaInfo bll = new TravletAgence.BLL.VisaInfo();
+
+
+            dataGridView1.DataSource = bll.GetListByPage(1, 30);
+
+            dataGridView1.m
+
+        }
+    }
+}
